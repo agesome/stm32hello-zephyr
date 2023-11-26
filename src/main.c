95c43d80502be5e394d4c316a04bca071a5fdd9d
Yevhenii Hrabliuk <github@nii.li>
2023-11-26 23:49:22 +0200

    use max7219

--- src/main.c
+++ src/main.c
@@ -7,19 +7,42 @@
 #include <zephyr/fs/fs.h>
 
 #include <zephyr/drivers/display.h>
+#include <errno.h>
 
 LOG_MODULE_REGISTER(main);
 
+void format_n(uint32_t n, uint8_t *buf, size_t bufsz)
+{
+	const uint8_t table[] = {126, 48, 109, 121, 51, 91, 95, 112, 127, 123};
+	for (size_t i = 0; i < bufsz; ++i)
+	{
+		buf[i] = table[n % 10];
+		n /= 10;
+	}
+}
+
 int main(void)
 {
-	const struct device *display_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
+	const struct device *display_dev = DEVICE_DT_GET(DT_NODELABEL(max7219));
 	if (!device_is_ready(display_dev))
 	{
 		LOG_ERR("display not ready");
 	}
-	else
+
+	struct display_buffer_descriptor buf_desc = 
 	{
-		LOG_INF("display seems ok?");
+		.buf_size = 8,
+		.height = 8,
+		.width = 8,
+		.pitch = 8
+	};
+
+	uint8_t buf[8];
+
+	for (;;)
+	{
+		format_n(k_uptime_get(), buf, sizeof(buf));
+		display_write(display_dev, 0, 0, &buf_desc, buf);
 	}
 
 	return 0;
